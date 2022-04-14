309b1a4bdab264d0f5eac8ce4e1274fe7d6db1e3
Abin Simon <mail@meain.io>
2022-04-14 16:50:52 +0530

    Add warning about DEV mode when calling userinfo

--- pkg/service/user.go
+++ pkg/service/user.go
@@ -466,6 +466,7 @@ func (s *userService) GetUserInfo(ctx context.Context, user *userv3.User) (*user
 	if s.dev {
 		username = user.GetMetadata().GetName()
 		if len(username) == 0 {
+			fmt.Println("Unable to fetch username. Don't use DEV mode when using from UI.")
 			return &userv3.UserInfo{}, fmt.Errorf("username should be provided")
 		}
 	} else {
