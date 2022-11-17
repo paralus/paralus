6b979d08081d3a443c9590b2e3aa401d118a17d0
nirav-rafay <93963752+nirav-rafay@users.noreply.github.com>
2022-11-17 14:20:56 +0530

    Update swagger-initializer.js

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -4,8 +4,9 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     urls: [   
-        { url: "https://github.com/paralus/paralus/blob/gh-pages/apis/partner.swagger.json", name: "Partner" },  
-        { url: "https://github.com/paralus/paralus/blob/gh-pages/apis/organization.swagger.json", name: "Organization" }  
+        { url: "https://paralus.github.io/paralus/apis/partner.swagger.json", name: "Partner" },  
+        { url: "https://paralus.github.io/paralus/apis/Organization.swagger.json", name: "Organization" },
+        { url: "https://paralus.github.io/paralus/apis/Project.swagger.json", name: "Project" },
     ],  
     "urls.primaryName": "Partner",
     dom_id: '#swagger-ui',
