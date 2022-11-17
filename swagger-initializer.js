536011d9202da83f9a54ec285b8b9f33f115fcb4
nirav-rafay <nirav-rafay@users.noreply.github.com>
2022-11-17 09:15:03 +0000

    deploy: 733b0a8de53df1c78acb310eedfde6b514ad68d7

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,20 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    urls: [   
-        { url: "https://paralus.github.io/paralus/apis/partner.swagger.json", name: "Partner" },  
-        { url: "https://paralus.github.io/paralus/apis/organization.swagger.json", name: "Organization" },
-        { url: "https://paralus.github.io/paralus/apis/project.swagger.json", name: "Project" },
-        { url: "https://paralus.github.io/paralus/apis/cluster.swagger.json", name: "Cluster" },
-        { url: "https://paralus.github.io/paralus/apis/user.swagger.json", name: "User" },
-        { url: "https://paralus.github.io/paralus/apis/group.swagger.json", name: "Group" },
-        { url: "https://paralus.github.io/paralus/apis/role.swagger.json", name: "Role" },
-        { url: "https://paralus.github.io/paralus/apis/rolepermission.swagger.json", name: "RolePermission" },
-        { url: "https://paralus.github.io/paralus/apis/kubectl_cluster.swagger.json", name: "KubectlSettings" },
-        { url: "https://paralus.github.io/paralus/apis/oidc_provider.swagger.json", name: "OIDCProvider" },
-        { url: "https://paralus.github.io/paralus/apis/auditlog.swagger.json", name: "AuditLog" },
-    ],  
-    "urls.primaryName": "Partner",
+    url: "https://petstore.swagger.io/v2/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
