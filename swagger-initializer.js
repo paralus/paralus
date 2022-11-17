fa8b565ff983aa141c44c9accc4d35678bccfb81
nirav-rafay <93963752+nirav-rafay@users.noreply.github.com>
2022-11-17 14:32:53 +0530

    Update swagger-initializer.js

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -5,8 +5,16 @@ window.onload = function() {
   window.ui = SwaggerUIBundle({
     urls: [   
         { url: "https://paralus.github.io/paralus/apis/partner.swagger.json", name: "Partner" },  
-        { url: "https://paralus.github.io/paralus/apis/Organization.swagger.json", name: "Organization" },
-        { url: "https://paralus.github.io/paralus/apis/Project.swagger.json", name: "Project" },
+        { url: "https://paralus.github.io/paralus/apis/organization.swagger.json", name: "Organization" },
+        { url: "https://paralus.github.io/paralus/apis/project.swagger.json", name: "Project" },
+        { url: "https://paralus.github.io/paralus/apis/cluster.swagger.json", name: "Cluster" },
+        { url: "https://paralus.github.io/paralus/apis/user.swagger.json", name: "User" },
+        { url: "https://paralus.github.io/paralus/apis/group.swagger.json", name: "Group" },
+        { url: "https://paralus.github.io/paralus/apis/role.swagger.json", name: "Role" },
+        { url: "https://paralus.github.io/paralus/apis/rolepermission.swagger.json", name: "RolePermission" },
+        { url: "https://paralus.github.io/paralus/apis/kubectl_cluster.swagger.json", name: "KubectlSettings" },
+        { url: "https://paralus.github.io/paralus/apis/oidc_provider.swagger.json", name: "OIDCProvider" },
+        { url: "https://paralus.github.io/paralus/apis/auditlog.swagger.json", name: "AuditLog" },
     ],  
     "urls.primaryName": "Partner",
     dom_id: '#swagger-ui',
